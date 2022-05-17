4b73c0ace9802972f30ac421b0cd1791b69ebc76
bharadwajpudi <bharadwajpudi2018@gmail.com>
2022-05-17 09:39:06 +0530

    Done

---
+++ firstrestday46/src/main/java/com/jobiak/empapi/model/Mobile.java
@@ -0,0 +1,57 @@
+package com.jobiak.empapi.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Mobile 
+{
+	@Id
+	@GeneratedValue
+	private Long mid;
+	private String brand;
+	private String model;
+	private double price;
+	public Mobile()
+	{
+		
+	}
+	
+	public Long getMid() {
+		return mid;
+	}
+
+	public void setMid(Long mid) {
+		this.mid = mid;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public Mobile(Long mid, String brand, String model, double price) {
+		super();
+		this.mid = mid;
+		this.brand = brand;
+		this.model = model;
+		this.price = price;
+	}
+	
+}
---
+++ firstrestday46/src/main/java/com/jobiak/empapi/resources/Product.java
@@ -0,0 +1,73 @@
+package com.jobiak.empapi.resources;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.jobiak.empapi.model.Mobile;
+import com.jobiak.empapi.service.MobileService;
+
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/catalog")
+public class Product 
+{
+	@Autowired
+	MobileService service;
+	
+	@GetMapping(value="/display",produces=MediaType.APPLICATION_JSON_VALUE)
+	public List<Mobile>showAll(){
+		return service.getAll();
+	}
+	
+	@GetMapping(value="/mobile",produces=MediaType.APPLICATION_JSON_VALUE)
+	public Mobile showCatalog()
+	{
+		Mobile mobile = new Mobile();
+		mobile.setBrand("Samsung");
+		mobile.setModel("E2FH,Dual Camera,32");
+		mobile.setPrice(49999);
+		return mobile;
+	}
+	@GetMapping("/intro")
+	public String introduction()
+	{
+		return new String("I am good");
+	}
+	
+	@GetMapping("/mobile/{modelId}")
+	public String searchModel(@PathVariable(value="modelId")String modelId)
+	{
+		return new String(modelId+"Is availabel in Blue and Black color can be delivered in 24 hours");
+	}
+	@GetMapping("/mobile/{modelId}/brand/{brand}")
+	public String searchModel(@PathVariable(value="modelId")String modelId,@PathVariable(value="brand")String brand)
+	{
+		return new String(modelId+brand+"Currently not avaiable for your loacation");
+	}
+	@PostMapping(value="/create",consumes=MediaType.APPLICATION_JSON_VALUE,produces=MediaType.APPLICATION_JSON_VALUE)
+	public Mobile addProduct(@RequestBody Mobile mobile)
+	{
+		//System.out.println(mobile);
+		Mobile ref = service.addMobile(mobile);
+		return ref;
+	}
+	@PutMapping(value="/modify",consumes=MediaType.APPLICATION_JSON_VALUE,produces=MediaType.APPLICATION_JSON_VALUE)
+	public Mobile modifyProduct(@RequestBody Mobile mobile) {
+		Mobile ref=service.modifyMobile(mobile);
+		return ref;
+	}
+	@DeleteMapping(value="/remove/{mid}")
+	public void removeProduct(@PathVariable(value="mid")Long mid) {
+		service.removeProduct(mid);
+	}
+}
